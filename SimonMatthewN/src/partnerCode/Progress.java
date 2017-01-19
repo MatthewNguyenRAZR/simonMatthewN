48a6429363c5d6f47f3d3839e329101b2bb3ee68
MatthewNguyenRAZR <matthewpdn@gmail.com>
2017-01-18 19:29:08 -0500

    Direct Update

--- SimonMatthewN/src/partnerCode/Progress.java
+++ SimonMatthewN/src/partnerCode/Progress.java
@@ -34,16 +34,16 @@ public class Progress extends Component implements ProgressInterfaceMatthew {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		FontMetrics fm = g.getFontMetrics();
 		if (gameOver) {
-			g.setColor(new Color(255, 55, 90));
-			g.fillOval(0, 0, W, H);
+			g.setColor(new Color(255, 150, 150));
+			g.fillRect(0, 0, W, H);
 			g.setColor(Color.white);
 			String go = "GAME OVER!";
 			g.drawString(go, (W - fm.stringWidth(go)) / 2, 20);
 			g.drawString(sequence, (W - fm.stringWidth(sequence)) / 2, 40);
 
 		} else {
-			g.setColor(new Color(220, 255, 230));
-			g.fillOval(0, 0, W, H);
+			g.setColor(new Color(150, 255, 150));
+			g.fillRect(0, 0, W, H);
 			g.setColor(Color.black);
 			if (round != null && sequence != null) {
 
